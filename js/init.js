c06049d9e489185c7656a018c6fae3410e70c863
Daemon <zhangfuchuan@bizcent.com>
2020-08-18 13:52:23 +0800

    img page

--- js/init.js
+++ js/init.js
@@ -20,7 +20,7 @@
 	
 	// Remove "loading" class once the page has fully loaded.
 		window.onload = function() {
-			document.body.className = '';
+			document.body.className = 'loading';
 		}
 
 	// Prevent scrolling on touch.
